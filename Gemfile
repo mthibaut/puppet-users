8872bc1e93805ca3ab2202633a22f34b3ff19e37
Maarten Thibaut <mthibaut@cisco.com>
2013-03-22 10:06:29 -0400

    Fixes for CI.

--- Gemfile
+++ Gemfile
@@ -1,13 +1,19 @@
-source :rubygems
+source 'http://rubygems.org'
 
 group :development, :test do
   gem 'puppetlabs_spec_helper', :require => false
+  gem 'rspec-hiera-puppet', :require => false
+  gem 'hiera', :require => true
+  gem 'hiera-puppet', :require => true
+
 end
 
 if puppetversion = ENV['PUPPET_GEM_VERSION']
   gem 'puppet', puppetversion, :require => false
-  if puppetversion < '3.0.0'
+  if /^[012]/.match(puppetversion)
+    gem 'hiera', :require => true
     gem 'hiera-puppet', :require => true
+    gem 'rspec-hiera-puppet'
   end
 else
   gem 'puppet', :require => false
--- spec/defines/users_spec.rb
+++ spec/defines/users_spec.rb
@@ -1,9 +1,11 @@
 #!/usr/bin/env rspec
+#require 'ruby-debug'
+#require 'pp'
 require 'spec_helper'
 
 describe 'users', :type => :define do
 
-  describe 'test simple user hash' do
+  describe 'directly specified simple user hash - no hiera' do
 
     let :title do
       'bar'
@@ -15,5 +17,27 @@ describe 'users', :type => :define do
     it { should contain_users('bar') }
     it { should contain_users__setup('foo').with_hash({"foo"=>{"uid"=>"123"}}) }
     it { should contain_user('foo').with_uid(123) }
+  end
+
+
+  describe 'hiera specified simple user hash' do
+    let :title do
+      'bar'
+    end
+
+    let :hiera_data do
+      { 'users_bar' => {'foo' => { 'uid' => '123' } } }
+    end
+
+    it { should contain_users('bar') }
+    it 'should do it' do
+      #Puppet::Util::Log.level = :debug
+      #Puppet::Util::Log.newdestination(:console)
+      #debugger
+      should contain_users__setup('foo').with_hash({"foo"=>{"uid"=>"123"}})
+    end
+    it { should contain_users__setup('foo').with_hash({"foo"=>{"uid"=>"123"}}) }
+    it { should contain_user('foo').with_uid(123) }
+
   end
 end
