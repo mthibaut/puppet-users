85c9eb4bbca89630869cdd1f7159dae1a23b0186
Bastiaan Visser <bastiaan@bastiaanvisser.info>
2016-06-28 16:13:28 +0200

    Update ssh_authorized_keys.pp

--- manifests/ssh_authorized_keys.pp
+++ manifests/ssh_authorized_keys.pp
@@ -1,15 +1,14 @@
 define users::ssh_authorized_keys($user, $hash) {
  
-    if(!defined(ssh_authorized_keys[$user])) {
-	ssh_authorized_key { "${user}-${name}" :
-	    ensure   => $hash[$name]['ensure'],
-	    key      => $hash[$name]['key'],
-	    options  => $hash[$name]['options'],
-	    provider => $hash[$name]['provider'],
-	    target   => $hash[$name]['target'],
-	    type     => $hash[$name]['type'],
-	    user     => $user,
-	}
+    if(!defined(Ssh_authorized_key["${user}-${name}"])) {
+    ssh_authorized_key { "${user}-${name}" :
+      ensure   => $hash[$name]['ensure'],
+      key      => $hash[$name]['key'],
+      options  => $hash[$name]['options'],
+      provider => $hash[$name]['provider'],
+      target   => $hash[$name]['target'],
+      type     => $hash[$name]['type'],
+      user     => $user,
     }
-
+  }
 }
